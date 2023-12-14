2128585217bc1c8610511b9be95c467f31d2c6fd
Yohel D. Larrauri Pizarro <yohel.larrpizarro@gmail.com>
2023-12-14 08:20:11 -0300

    first commit - add dependencies to gradle

---
+++ app/src/main/java/br/edu/scl/ifsp/sdm/moviesmanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.scl.ifsp.sdm.moviesmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
